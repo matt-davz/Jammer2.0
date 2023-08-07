2c6a350da80258c353dc8768cec0e8f8f70f5b7b
matt-davz <davies.matt.02@gmail.com>
2023-08-07 10:29:50 +0700

    fleshed out App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,7 +6,12 @@ function App() {
    <div>
       <h1>Ja<span className="highlight">mm</span>ing</h1>
       <div className="App">
-
+        {/* SearchBar */}
+        <section className='playlist-app'>  
+          <div>
+            
+          </div>
+        </section>
       </div>
    </div>
   );
