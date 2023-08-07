e6b940af3fee1bfaf14cb588e7c0dc2bbb4bfd0c
matt-davz <davies.matt.02@gmail.com>
2023-08-07 10:10:26 +0700

    removed bloat and added appropiate filings

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import './App.css';
+
+function App() {
+  return (
+   <div>
+      <h1>Ja<span className="highlight">mm</span>ing</h1>
+      <div className="App">
+
+      </div>
+   </div>
+  );
+}
+
+export default App;
