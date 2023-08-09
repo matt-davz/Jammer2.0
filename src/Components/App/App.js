136d4310e5d49800f3322cea7b524af2831d4161
matt-davz <davies.matt.02@gmail.com>
2023-08-09 09:41:39 +0700

    api is working finally

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,47 +3,19 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import PlayList from '../PlayList/PlayList';
 import SearchResults from '../SearchResults/SearchResults';
+import Spotify from '../../Utility/utility';
 
-const mockObj = [ 
-  {
-    id:1,
-    track:"Wriggle",
-    artist:"Cosmo Waldrek",
-    album:"Wriggle Delux"
-  },
-  {
-    id:2,
-    track:"Light of Day",
-    artist:"ODESZA",
-    album:"ODESZA FINAL"
-  },
-  {
-    id:3,
-    track:"7 Years Old",
-    artist:"Luke Graham",
-    album:"7 Years Old Deluxe"
-  },
-  {
-    id:4,
-    track:"Drop It Like Its Hot",
-    artist:"Snoop Dawg",
-    album:"Str8 Outta Compt"
-  },
-  {
-    id:5,
-    track:"Drop It Like Its Hot",
-    artist:"Snoop Dawg",
-    album:"Str8 Outta Compt"
-  }
-]
 
 function App() {
-  const [searchResults,setSearchResults] = useState(mockObj) //this gets the object array from the API set in handleSearch
+  const [searchResults,setSearchResults] = useState([]) //this gets the object array from the API set in handleSearch
   const [playListName,setPlayListName] = useState("New Playlist")
   const [addToPlayList,setAddToPLaylist] = useState([])
   const handleSearch = (term) => { //takes input from <SearchBar> and stores it in searchResearch
-    // spotify api .term.then(setSearchResults) ------!
-    setSearchResults(mockObj)
+    Spotify.search(term).then(results => {
+      setSearchResults(results)
+    })
+  
+
   }
 
   const changePlaylistName = (name) => {
--- src/Utility/utility.js
+++ src/Utility/utility.js
@@ -1,6 +1,63 @@
-const clientId = "id here"
-const code = undefined
+const client_id = "868195f0bf0e479e98c8bc76070a3438";
+const redirectUri = 'http://localhost:3000/';
+const client_secret = 'd91dc264ebae42d495e3e1b57f050075';
 
-if(!code){
-    redirectToAuthCodeFlow(clientId)
-}
+const Spotify = {
+  getAccessToken() {
+    const authHeader = 'Basic ' + btoa(`${client_id}:${client_secret}`);
+    const authOptions = {
+      method: 'POST',
+      headers: {
+        'Authorization': authHeader,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'grant_type=client_credentials',
+    };
+
+    return fetch('https://accounts.spotify.com/api/token', authOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed with status:', response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const token = data.access_token;
+        return token;
+      })
+      .catch(error => {
+        console.error('Error:', error.message);
+      });
+  },
+
+  search(term) {
+    return this.getAccessToken().then(accessToken => {
+      return fetch(`https://api.spotify.com/v1/search?q=${term}&type=track`, {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(jsonResponse => {
+          if (!jsonResponse) {
+            return [];
+          } else {
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                track: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name
+            }))
+          }
+        });
+    });
+  },
+
+  savePlaylist(name,tracks,userID) {
+    
+  }
+};
+
+
+
+export default Spotify
